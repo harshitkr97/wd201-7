d6855a22c3571d24dc85f88bc3766fe3ecd6f57c
harshitkr97 <77691812+harshitkr97@users.noreply.github.com>
2023-01-04 21:43:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('./app');
-app.listen(process.env.PORT || 3000, ()=>{
-  console.log('started express server');
+const app = require("./app");
+
+app.listen(3000, () => {
+  console.log("Started express server at port 3000");
 });
